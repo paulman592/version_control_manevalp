7a313688d4ded1d7ab1f926a03defcf529919797
Paul Maneval <paul.maneval@mac.com>
2016-11-21 21:43:12 -0500

    added function to capitalize bases and find proportion of GC content in sequence

--- houseelf_data.R
+++ houseelf_data.R
@@ -1,1 +1,14 @@
-houseelf_data <- read.csv("houseelf-earlength-dna-data_2.csv")
+library(stringr)
+
+houseelf_data <- read.csv("houseelf-earlength-dna-data_1.csv")
+get_gc_content <- function(seq){
+  Gs <- str_to_upper(seq,"g")
+  Cs <- str_to_upper(seq,"g")
+  Gs <- str_count(Gs,"G")
+  Cs <- str_count(Cs,"C")
+  gc_content <- (Gs + Cs) / str_length(seq) * 100
+  return(gc_content)
+}
+
+GC_prop <- get_gc_content(houseelf_data$dnaseq)
+GC_prop
