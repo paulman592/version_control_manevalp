ff7826cd69ddefe5bc925e64b93fd151772fbead
Ethan White <ethan@weecology.org>
2016-11-21 22:19:56 -0500

    Add function for determining ear length class

--- houseelf_data.R
+++ houseelf_data.R
@@ -10,6 +10,12 @@ get_gc_content <- function(seq){
   return(gc_content)
 }
 
+get_ear_length <- function(seq){
+    #Determine the ear length category
+    ear_lengths <- ifelse(seq > 10, "large", "small")
+    return(ear_lengths)
+}
+
 GC_prop <- get_gc_content(houseelf_data$dnaseq)
 GC_prop
 
